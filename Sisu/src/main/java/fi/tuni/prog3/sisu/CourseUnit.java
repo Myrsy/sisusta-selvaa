b0132d2f08d7e357cccb8017da61bd04ae455e74
Väinö Anttalainen <vaino.anttalainen@tuni.fi>
2022-04-16 13:16:21 +0300

    CourseUnit-luokkaan lisätty muuttujia ja getterit

--- Sisu/src/main/java/fi/tuni/prog3/sisu/CourseUnit.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/CourseUnit.java
@@ -11,8 +11,55 @@ package fi.tuni.prog3.sisu;
  */
 public class CourseUnit {
     
-    public CourseUnit(String name, String code){
-        
+    private String name;
+    private String groupId;
+    private String code;
+    private int minCredits;
+    private int maxCredits;
+    private String content;
+    private String outcomes;
+    private String gradeScaleId;
+
+    public CourseUnit(String name, String groupId, String code, int minCredits, 
+            int maxCredits, String content, String outcomes, String gradeScaleId) {
+        this.name = name;
+        this.groupId = groupId;
+        this.code = code;
+        this.minCredits = minCredits;
+        this.maxCredits = maxCredits;
+        this.content = content;
+        this.outcomes = outcomes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getMinCredits() {
+        return minCredits;
+    }
+
+    public int getMaxCredits() {
+        return maxCredits;
     }
     
+    public String getContent() {
+        return content;
+    }
+
+    public String getOutcomes() {
+        return outcomes;
+    }
+    
+    public String getGradeScaleId() {
+        return gradeScaleId;
+    }
 }
