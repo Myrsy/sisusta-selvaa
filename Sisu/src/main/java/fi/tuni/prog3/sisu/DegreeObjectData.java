ecf33a06aee6148f5a42effa066a2dcdeb985f54
Väinö Anttalainen <vaino.anttalainen@tuni.fi>
2022-04-25 12:04:41 +0300

    DegreeObjectData-luokka tallentaa tutkinnot HashMappiin

--- Sisu/src/main/java/fi/tuni/prog3/sisu/DegreeObjectData.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/DegreeObjectData.java
@@ -8,31 +8,41 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class DegreeObjectData {
     
     private static final String FULL_DEGREES_FILENAME = "fulldegreesfile.txt";
-    private DegreeProgramme[] degreeObjs;
+    private static final String OBJECTS_TO_JSON_FILENAME = "objsJson.txt";
     private HashMap<String, DegreeProgramme> degreeProgrammes;
 
     public DegreeObjectData() {
         this.degreeProgrammes = new HashMap<>();
     }
     
-    public void JsonToObjects() throws FileNotFoundException, IOException {
+    public void jsonToObjects() throws FileNotFoundException, IOException {
         
         Gson gson = new Gson();
         try (Reader reader = new FileReader(FULL_DEGREES_FILENAME)) {
-           degreeObjs = gson.fromJson(reader, DegreeProgramme[].class);
-        }   
+           DegreeProgramme[] progs = gson.fromJson(reader, DegreeProgramme[].class);
+           for (DegreeProgramme prog: progs) {
+               System.out.println(prog.getName());
+               addProgramme(prog);
+           }
+        }
     }
     
-    public void writeObjsToJson() throws IOException {     
+    public void objectsToJson() throws IOException {     
                 
-        try (FileWriter fw = new FileWriter("objsJson.txt", Charset.forName("UTF-8"))){
+        try (FileWriter fw = new FileWriter(OBJECTS_TO_JSON_FILENAME, Charset.forName("UTF-8"))){
+            ArrayList<DegreeProgramme> degreeObjs = new ArrayList<>();
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            for (String id: degreeProgrammes.keySet()) {
+                System.out.println(id);
+                degreeObjs.add(degreeProgrammes.get(id));
+            }
             gson.toJson(degreeObjs, fw);
             
         }
