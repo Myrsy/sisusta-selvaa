f5c916b1accfed2306b3d6b12d0a18c130b435de
pinjamyl <pinja.myllymaki@tuni.fi>
2022-04-26 13:17:18 +0300

    Pieniä muutoksia CourseUnit luokkaan

--- Sisu/src/main/java/fi/tuni/prog3/sisu/CourseUnit.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/CourseUnit.java
@@ -15,18 +15,16 @@ public class CourseUnit {
     private String groupId;
     private String code;
     private int minCredits;
-    private int maxCredits;
     private String content;
     private String outcomes;
     private String gradeScaleId;
 
     public CourseUnit(String name, String groupId, String code, int minCredits, 
-            int maxCredits, String content, String outcomes, String gradeScaleId) {
+             String content, String outcomes, String gradeScaleId) {
         this.name = name;
         this.groupId = groupId;
         this.code = code;
         this.minCredits = minCredits;
-        this.maxCredits = maxCredits;
         this.content = content;
         this.outcomes = outcomes;
     }
@@ -45,10 +43,6 @@ public class CourseUnit {
 
     public int getMinCredits() {
         return minCredits;
-    }
-
-    public int getMaxCredits() {
-        return maxCredits;
     }
     
     public String getContent() {
