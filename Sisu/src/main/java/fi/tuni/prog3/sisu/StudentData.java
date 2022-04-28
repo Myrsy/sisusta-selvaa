51fdffaf59d4a80a7d031ef20ab872444c55b408
Väinö Anttalainen <vaino.anttalainen@tuni.fi>
2022-04-28 19:57:36 +0300

    StudentData-luokka kirjoittaa opiskelijoita tiedostoon

--- Sisu/src/main/java/fi/tuni/prog3/sisu/StudentData.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/StudentData.java
@@ -2,6 +2,12 @@
 package fi.tuni.prog3.sisu;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -11,14 +17,16 @@ import java.util.HashMap;
  */
 public class StudentData {
     
+    private static final String STUDENTS_TO_JSON_FILENAME = "sudentsfile.txt";
     private HashMap<String, Student> students;
     
     public StudentData(){
         this.students = new HashMap<>();
     }
     
-    public void addStudent(String studentNumber, Student student){
-        this.students.put(studentNumber, student);
+    public void addStudent(Student student) throws IOException{
+        this.students.put(student.getStudentNumber(), student);
+        studentsToFile();
     }
     
     public boolean searchStudent(String studentNumber){
@@ -30,6 +38,19 @@ public class StudentData {
     
     public void getOldStudents(){
         
+    }
+    
+    public void studentsToFile() throws IOException {
+        
+        try (FileWriter fw = new FileWriter(STUDENTS_TO_JSON_FILENAME, Charset.forName("UTF-8"))){
+            ArrayList<Student> studentObjs = new ArrayList<>();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            for (String id: students.keySet()) {
+                studentObjs.add(students.get(id));
+            }
+            gson.toJson(studentObjs, fw);
+            
+        }
     }
     
 }
